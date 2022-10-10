4eec64628bf4b1a4b6fd75fb1d2df1610b5a6c84
mracoon <nataliyakomarova777@gmail.com>
2022-10-10 14:31:06 +0300

    feat: add solution for Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,83 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isReverse) {
+    this.dir = !(isReverse === false)
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    const alph = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    if (arguments.length < 2 || arguments[0] === undefined || arguments[1] === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    let messArr = []
+    let keyArr = []
+    let keyArrEnc = []
+    let encrCodeArr = []
+    let keyIndex = 0
+
+    for (let i = 0; i < key.length; i++) { keyArr.push(alph.indexOf(key[i])) }
+
+    for (let i = 0; i < message.length; i++) {
+      if (alph.indexOf(message[i]) !== -1) {
+        messArr.push(alph.indexOf(message[i]))
+        keyArrEnc.push(keyArr[keyIndex])
+        let x = alph.indexOf(message[i]) + keyArr[keyIndex]
+        if (x > 25) { x = x - 26 } //сложение по модулю 26 (длина alph)
+        encrCodeArr.push(alph[x])
+        keyIndex += 1
+      }
+      else {
+        messArr.push(`${message[i]}`)
+        keyArrEnc.push('')
+        encrCodeArr.push(`${message[i]}`)
+      }
+
+      if (keyIndex > key.length - 1) { keyIndex = 0 }
+    }
+
+    if (this.dir) { return encrCodeArr.join('') }
+    else { return (encrCodeArr.reverse()).join('') }
+  }
+
+  decrypt(message, key) {
+    const alph = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    if (arguments.length < 2 || arguments[0] === undefined || arguments[1] === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+
+    let messArr = []
+    let keyArr = []
+    let keyArrEnc = []
+    let encrCodeArr = []
+    let keyIndex = 0
+
+    for (let i = 0; i < key.length; i++) { keyArr.push(alph.indexOf(key[i])) }
+
+    for (let i = 0; i < message.length; i++) {
+      if (alph.indexOf(message[i]) !== -1) {
+        messArr.push(alph.indexOf(message[i]))
+        keyArrEnc.push(keyArr[keyIndex])
+        let x = alph.indexOf(message[i]) - keyArr[keyIndex]
+        if (x < 0) { x = 26 + x } //вычитание  по модулю 26 (длина alph)
+        encrCodeArr.push(alph[x])
+        keyIndex += 1
+      }
+      else {
+        messArr.push(`${message[i]}`)
+        keyArrEnc.push('')
+        encrCodeArr.push(`${message[i]}`)
+      }
+
+      if (keyIndex > key.length - 1) { keyIndex = 0 }
+    }
+
+    if (this.dir) { return encrCodeArr.join('') }
+    else { return (encrCodeArr.reverse()).join('') }
   }
 }
 
