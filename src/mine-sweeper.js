3cd4a0fc323f24274651d9703f90787e43ff47d7
mracoon <nataliyakomarova777@gmail.com>
2022-10-11 22:57:03 +0300

    feat: add solution for Minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rows = matrix.length - 1
+  const cols = matrix[0].length - 1
+  let res = []
+  let rowMines = []
+  for (let i = 0; i < matrix.length; i++) {
+    rowMines = []
+    for (let j = 0; j < matrix[i].length; j++) {
+
+      let mines = 0
+      const fullMine = (i !== 0) && (j !== 0) && (i !== rows) && (j !== cols)
+      const topCenterMine = (i === 0) && (j !== 0) && (j !== cols)
+      const botomCenterMine = (i === rows) && (j !== 0) && (j !== cols)
+      const leftCenterMine = (i !== 0) && (j === 0) && (i !== rows)
+      const rightCenterMine = (i !== 0) && (i !== rows) && (j === cols)
+
+      if (i === 0 || fullMine || topCenterMine || leftCenterMine || rightCenterMine) {
+        matrix[i + 1][j] === true ? mines += 1 : 0
+        if (j === 0 || fullMine || topCenterMine || leftCenterMine) {
+          matrix[i][j + 1] === true ? mines += 1 : 0
+          matrix[i + 1][j + 1] === true ? mines += 1 : 0
+        }
+        if (j === cols || fullMine || topCenterMine || rightCenterMine) {
+          matrix[i][j - 1] === true ? mines += 1 : 0
+          matrix[i + 1][j - 1] === true ? mines += 1 : 0
+        }
+
+      }
+
+      if (i === rows || fullMine || botomCenterMine || leftCenterMine || rightCenterMine) {
+        matrix[i - 1][j] === true ? mines += 1 : 0
+        if ((j === 0 || botomCenterMine) && (!leftCenterMine)) {
+          matrix[i][j + 1] === true ? mines += 1 : 0
+          matrix[i - 1][j + 1] === true ? mines += 1 : 0
+        }
+        if ((j === cols || botomCenterMine) && (!rightCenterMine)) {
+          matrix[i][j - 1] === true ? mines += 1 : 0
+          matrix[i - 1][j - 1] === true ? mines += 1 : 0
+        }
+
+      }
+      if (leftCenterMine) {
+        matrix[i - 1][j + 1] === true ? mines += 1 : 0
+      }
+
+      if (rightCenterMine) {
+        matrix[i - 1][j - 1] === true ? mines += 1 : 0
+      }
+      if (fullMine) {
+        matrix[i - 1][j + 1] === true ? mines += 1 : 0
+        matrix[i - 1][j - 1] === true ? mines += 1 : 0
+      }
+      rowMines.push(mines)
+
+    }
+    res.push(rowMines)
+  }
+  return res
 }
 
 module.exports = {
